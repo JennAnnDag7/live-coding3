85a12f0625a8f4c82a71291b6ba4f68b71d053a9
JennAnnDag7 <54606848+JennAnnDag7@users.noreply.github.com>
2019-12-03 19:14:45 -0500

    Add files via upload
    livecoding

---
+++ Live-coding3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Live_coding3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int array_one = [1, 5, 2];
+            int array_two = [3, 6, 4];
+
+            int zipped_array = " ";
+           
+            Console.WriteLine(zipped_array);
+                                             
+        }
+
+        public class zip()
+        {
+            foreach 
+        }
+    }
+}
